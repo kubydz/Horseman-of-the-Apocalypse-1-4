d54acc8ef6b438ff1eb32e3d3230b8ea621ed24b
kubydz <kubydonmez@icloud.com>
2025-01-20 11:04:04 +0100

    update 3

--- public/main.js
+++ public/main.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const counters = document.querySelectorAll(".counter")
-    const maxCounts = [1800, 59500, 38000] // Max Followers //
+    let counters = document.querySelectorAll(".counter")
+    let maxCounts = [1800, 59500, 38000] // Max Followers //
   
     counters.forEach((counter, index) => {
       let count = 0
-      const maxCount = maxCounts[index]
-      const duration = 3000 // 3 seconds //
-      const interval = 20 // Update //
-      const increment = maxCount / (duration / interval)
+      let maxCount = maxCounts[index]
+      let duration = 3000 // 3 seconds //
+      let interval = 20 // Update //
+      let increment = maxCount / (duration / interval)
   
-      const updateCounter = () => {
+      let updateCounter = () => {
         count += increment
         if (count > maxCount) {
           count = maxCount
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
         counter.textContent = Math.round(count)
       }
   
-      const timer = setInterval(updateCounter, interval)
+      let timer = setInterval(updateCounter, interval)
     })
   })
   
