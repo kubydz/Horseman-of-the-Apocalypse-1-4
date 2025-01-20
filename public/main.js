b554ddf46aa16beb5e6e884882a3803f38588017
kubydz <kubydonmez@icloud.com>
2025-01-20 10:16:22 +0100

    js fonctionnel

---
+++ public/main.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const counters = document.querySelectorAll(".counter")
+    const maxCounts = [1000, 5000, 3000] // Max Followers //
+  
+    counters.forEach((counter, index) => {
+      let count = 0
+      const maxCount = maxCounts[index]
+      const duration = 3000 // 3 seconds //
+      const interval = 20 // Update //
+      const increment = maxCount / (duration / interval)
+  
+      const updateCounter = () => {
+        count += increment
+        if (count > maxCount) {
+          count = maxCount
+          clearInterval(timer)
+        }
+        counter.textContent = Math.round(count)
+      }
+  
+      const timer = setInterval(updateCounter, interval)
+    })
+  })
+  
